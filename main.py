d8f2bfec690f9e5c72aa09c9728ea260c1bf9a8a
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-08-19 22:50:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,11 @@ app=Flask(__name__)
 @app.route("/question",methods=['POST'])
 def index():
     sentence=request.json['work']
-    r=response.question(sentence)
-    return {"result":r}
-@app.route("/task1",methods=['POST'])
-def index1():
-    task1=request.json['task']
     deadline=request.json['deadline']
-    r =task.link(task1,deadline)
-    return r
+    id=request.json["id"]
+    r=response.question(sentence)
+    task.link(sentence,deadline,id)
+    return {"result":r}
 
 if __name__=="__main__":
     app.run(debug=True,port=6000)
