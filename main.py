ea63a7402a9100211b8791925317e88453e8595a
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-08-19 19:27:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import *
-import response
+import response,task
 app=Flask(__name__)
 
 
@@ -8,6 +8,12 @@ def index():
     sentence=request.json['work']
     r=response.question(sentence)
     return {"result":r}
+@app.route("/task1",methods=['POST'])
+def index1():
+    task1=request.json['task']
+    deadline=request.json['deadline']
+    r =task.link(task1,deadline)
+    return r
 
 if __name__=="__main__":
     app.run(debug=True,port=6000)
