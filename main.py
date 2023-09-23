9932571858c465a31a10477a33c1d757db226c9d
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-09-23 10:49:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from flask import *
-import response,task
 app=Flask(__name__)
+import firebase,pdf
 
 
-@app.route("/question",methods=['POST'])
+@app.route("/",methods=['POST'])
 def index():
-    sentence=request.json['work']
-    deadline=request.json['deadline']
-    id=request.json["id"]
-    tech_needed=response.question(sentence)
-    r=task.link(tech_needed,sentence,deadline,id)
-    return {"result":r}
+    name=request.json['name']
+    r=firebase.done(name)
+    if r==True:
+        word=pdf.pdfread(name)
+    return {"result":word}
 
 if __name__=="__main__":
     app.run(debug=True,port=6000)
