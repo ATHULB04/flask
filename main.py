252f779e70595c41f6f84178d8015c9e3c6b3256
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-09-24 01:23:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 from flask import *
-import os
+from fpdf import FPDF
 app=Flask(__name__)
-
-import pyrebase
-
-config ={
- "apiKey": "AIzaSyAvXSSHSTbm7vXR_0mySUwOPJWlvZ1E480",
-  "authDomain": "studiesy.firebaseapp.com",
-  "databaseURL": "https://studiesy-default-rtdb.firebaseio.com",
-  "projectId": "studiesy",
-  "storageBucket": "studiesy.appspot.com",
-  "messagingSenderId": "1070198266308",
-  "appId": "1:1070198266308:web:ee0546f9c31da90d256c91",
-  "measurementId": "G-SPW2YS1XML"
-}
-firebase = pyrebase.initialize_app(config)
-
-
 
 @app.route("/",methods=['POST'])
 def index():
     name=request.json['name']
-    localpath=f"{name}.pdf"
-    cloudpath=f"test/{name}.pdf"
-    firebase.storage().child(cloudpath).download(filename="test.pdf")
-    return {"result":"done"}
+
+
+    f=FPDF()
+    f.add_page()
+
+    f.output(f"{name}.pdf")
+    return({"result":"done"})
 
 if __name__=="__main__":
     app.run(debug=True,port=6000)
