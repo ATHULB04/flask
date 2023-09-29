10c115029cd7b2a5cec5384c4700fb61ba956fd0
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-09-29 20:35:49 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,19 +1,38 @@
-import pyrebase
-import pdf
-config ={
- "apiKey": "AIzaSyAvXSSHSTbm7vXR_0mySUwOPJWlvZ1E480",
-  "authDomain": "studiesy.firebaseapp.com",
-  "databaseURL": "https://studiesy-default-rtdb.firebaseio.com",
-  "projectId": "studiesy",
-  "storageBucket": "studiesy.appspot.com",
-  "messagingSenderId": "1070198266308",
-  "appId": "1:1070198266308:web:ee0546f9c31da90d256c91",
-  "measurementId": "G-SPW2YS1XML"
-}
+import os
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+os.environ["OPENAI_API_KEY"] = os.environ.get('OPENAI_API_KEY')
+import firebase_admin
+from firebase_admin import credentials,firestore
 
-firebase = pyrebase.initialize_app(config)
-def done(name):
-    localpath=""
-    cloudpath="test/sample.pdf"
-    firebase.storage().child(cloudpath).download(localpath,filename="sample.pdf")
-    return True
+cred = credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(cred) 
+db=firestore.client()
+
+
+def prompt():
+    llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.1)
+
+    prompt = PromptTemplate(
+        input_variables=["food"],
+        template="write a detailed summary of {food} and explain it in a very understandable way.",
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    return chain
+
+def question1(inputs):
+    chain = prompt()
+    return chain.run(inputs)
+
+
+def details(subject,sentence):
+    data=question1(sentence)   #semd to gpt
+    print(2)
+    db.collection("Notes").document(subject).update({"summary":data})
+    return "done"
+
+
+
+
+
