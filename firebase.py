61909d77a4119831d1c9045177349b3722f3bf82
ATHUL BABU <113769666+ATHULB04@users.noreply.github.com>
2023-09-30 00:22:56 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -2,6 +2,9 @@ import os
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
+from langchain.schema import HumanMessage
+from langchain.chat_models import ChatOpenAI
+
 os.environ["OPENAI_API_KEY"] = os.environ.get('OPENAI_API_KEY')
 import firebase_admin
 from firebase_admin import credentials,firestore
@@ -10,20 +13,34 @@ cred = credentials.Certificate("serviceAccount.json")
 firebase_admin.initialize_app(cred) 
 db=firestore.client()
 
+response_cache = {}
+def promptmaker(transcribed_txt):
+    instructions = """You are a chatbot who helps students learn. You make a summary based on the teacher's words.
+    Only use words that the teacher told. Don't tell anything more or less than what the teacher told.
+    Make sure the summary is not too long or too short.
+    Make sure the summary is easy to understand.
+    Make sure the summary only contains the contents told by the teacher.
+    answer as a third person and remember you follow the format of a summary.
+    """
 
-def prompt():
-    llm = OpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0.1)
+    data = str(transcribed_txt)
+    question = f"give me a summary of {transcribed_txt}"  # Corrected the variable name
+    prompt = instructions + data + question
+    return askgpt(prompt)
 
-    prompt = PromptTemplate(
-        input_variables=["food"],
-        template="write a detailed summary of {food} and explain it in a very understandable way.",
-    )
-    chain = LLMChain(llm=llm, prompt=prompt)
-    return chain
+def askgpt(prompt):
+    
+    chat_model = ChatOpenAI(temperature=0.1, model='gpt-3.5-turbo', openai_api_key=os.environ.get("OPENAI_API_KEY"), max_tokens=250, stop=["\n"])
+    
+    if prompt in response_cache:
+        return response_cache[prompt]
 
-def question1(inputs):
-    chain = prompt()
-    return chain.run(inputs)
+    output = chat_model([HumanMessage(content=prompt)])
+    response = output.content
+    response_cache[prompt] = response
+    return response
+
+
 
 
 def details(subject):
@@ -32,14 +49,15 @@ def details(subject):
         list=db.collection("Notes").document(subject).get()
         data=list.to_dict()
         sentence=data.get('transcribe', '').strip()
-        print(2)
+        print(sentence)
     else:
         print("does not exist")
 
-    data=question1(sentence)   #semd to gpt
+    data=promptmaker(sentence)   #semd to gpt
     print(3)
     db.collection("Notes").document(subject).update({"summary":data})
     return "done"
+
 
 
 
